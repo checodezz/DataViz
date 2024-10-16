3e998e1ee72f30f8a64559a46cdade9ba28bb434
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-10-16 07:22:54 +0000

    add auth routes

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 import express from "express"
-import mongoose from "mongoose"
 import cors from "cors"
 import dotenv from "dotenv"
 import Router from "./routes/route.js"
 import connectDb from "./db/db.js"
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser())
 
 connectDb()
 
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,7 +1,10 @@
 import express from "express"
-import { userSigUp } from "../controllers/userController.js"
+import { userSigUp, userLogin, userDashboard} from "../controllers/userController.js"
+import auth from "../middleware/auth.js";
 const router = express.Router();
 
 router.post("/user/signup", userSigUp);
+router.post("/user/login",userLogin);
+router.get("/dashboard", auth, userDashboard)
 
 export default router
