e54bb33c9dc9c0a1bae79b78ec337243693d2eae
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-10-20 04:10:57 +0000

    fix

--- server/index.js
+++ server/index.js
@@ -1,49 +1,40 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-import Router from "./routes/route.js"
-import connectDb from "./db/db.js"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import Router from "./routes/route.js";
+import connectDb from "./db/db.js";
 import cookieParser from "cookie-parser";
-import { fetchSheetData } from "./dynamicSheetData/dataFetcher.js"
+import { fetchSheetData } from "./dynamicSheetData/dataFetcher.js";
 
 dotenv.config();
 
-const allowedOrigins = ["http://localhost:5173", "https://data-visualization-dashboard-psi.vercel.app/", "https://vercel.com/checodezzs-projects/data-visualization-dashboard/BA5UpaHHdfu8GSiqHR8N8pMsPTG6"];
-
 const app = express();
 
+// Set up CORS options to allow all origins
 const corsOptions = {
-    origin: function (origin, callback) {
-        console.log("Origin:", origin); // Log the origin for debugging
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true,
+    origin: true, // Allows all origins
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
     methods: ["GET", "POST", "OPTIONS"], // Allow these methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allow these headers
 };
 
-app.use(cors(corsOptions))
+// Use CORS middleware
+app.use(cors(corsOptions));
 app.options('*', cors(corsOptions)); 
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
-connectDb()
-fetchSheetData()
-
+connectDb();
+fetchSheetData();
 
 app.get('/', (req, res) => {
     res.send('Hello, welcome to dataWiz dashboard !');
 });
 
-app.use("/", Router)
+app.use("/", Router);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
-
